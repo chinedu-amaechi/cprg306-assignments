e8ba3c15c966e123eb78c8bade4c77ae8e68bb1e
Chinedu Amaechi <chymez56@yahoo.com>
2024-09-27 13:04:34 -0600

    Styling
    Additional styling requirement

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,13 +2,12 @@
 export default function Item({name, quantity, category}) {
 
      return (
-        <section>
-            <div className="m-4 w-1/3 bg-slate-300 pl-3 pb-1">
-            <ol>
-                <li className="text-xl font-sans font-bold">{name}</li>
-                <li>Buy {quantity} in {category}</li>
-            </ol>
-            </div>
-        </section>
+        
+            <li className="m-3 max-w-sm bg-slate-300 pl-3 pb-1">
+                <h2 className="text-xl font-sans font-bold">{name}</h2>
+                <div>Buy {quantity} in {category}</div>
+            </li>
+            
+        
     );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,15 @@
 import ItemList from "./item-list";
 
-
 export default function Page() {
-    return (
-        <main className="m-3">
-            <h1 className="font-extrabold text-3xl text-slate-900">Shopping List</h1>
-            <ItemList />
-        </main>
-    );
+  return (
+    <main className="m-1">
+      <div>
+        <h2 className=" m-2 font-extrabold text-3xl text-slate-900">
+          Shopping List
+        </h2>
+        <ItemList />
+      </div>
+      
+    </main>
+  );
 }
