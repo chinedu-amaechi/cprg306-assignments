2deb3fbfe12b18913c41c3fea9763bdf522f8df5
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-02 13:19:24 -0600

    Additional Styling and Button interactivity
    Updated the New-Item component to enhance the layout and functionality of quantity control buttons, including improved alignment, added input field, and button disable conditions.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -18,16 +18,17 @@ export default function NewItem() {
     };
 
   return (
-    <div className="flex p-1 m-4 bg-slate-600 text-slate-950 w-60">
+    <div className="flex items-center justify-between p-2 m-3 bg-slate-600 text-slate-950 w-36">
 
+        <span className="text-white text-xl">{quantity}</span>
+
+        <div className="flex items-center gap-2">
+          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={decrement} disabled={quantity==1}>-</button>
       
-        <span><input type="text" value={quantity} /></span>
-           
-        <button type="button" className="w-10 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={decrement}>-</button>
-     
-        <button type="button" className="w-10 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={increment}>+</button>
-     
-      
+          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={increment} disabled={quantity==20}>+</button>
+
+        </div>
+         
     </div>
         
   );
