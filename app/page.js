2edc899411220b27e540155e744116012fb3ed64
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-03 17:03:50 -0600

    Additional Styling
    Styling of the Landing Page.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 export default function Page() {
   return (
     <div className="m-3 ">
-      <h1>Welcome to Development II</h1>
-      <p>DEMOs for CPRG306 Assignment</p>
-      <ul>
-        <li ><a href="/week-2">Week 2</a></li>
-        <li><a href="/week-3">Week 3</a></li>
-        <li><a href="/week-4">Week 4</a></li>
-        <li><a href="/week-5">Week 5</a></li>
-      </ul>
+      <h1 className="font-extrabold text-4xl text-center text-red-700">Welcome to Development II</h1>
+      <hr className="m-2 border-2"/>
+      <div className="m-3">
+      <p className="font-semibold">DEMOs for CPRG306 Assignment</p>
+        <div className="flex w-full m-2 ml-0">
+          <ul className="flex gap-4 text-center font-semibold text-red-500 p-1 pl-0">
+            <li className="border-2 border-blue-950 w-28 h-8"><a href="/week-2">Week 2</a></li>
+            <li className="border-2 border-blue-950 w-28 h-8"><a href="/week-3">Week 3</a></li>
+            <li className="border-2 border-blue-950 w-28 h-8"><a href="/week-4">Week 4</a></li>
+            <li className="border-2 border-blue-950 w-28 h-8"><a href="/week-5">Week 5</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,7 +3,7 @@ import StudentInfo from "./student-info";
 export default function Page() {
   return (
     <main>
-        <h1>Shopping List | <a href="/"> Home Page</a></h1>
+        <h1>Shopping List | <a href="./"> Home Page</a></h1>
         <StudentInfo />
     </main>
   );
