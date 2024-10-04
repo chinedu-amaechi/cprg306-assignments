91f5787a9555fa9a9e1e94ccfe4d73efaf541573
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-04 12:51:32 -0600

    Enhance list item styles on landing page
    Updated the list items to use flexbox for layout. Modified the width and height of each list item and added padding for better spacing. Applied new text styles for better readability and visual appeal.

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,7 +3,7 @@ export default function Item({name, quantity, category}) {
 
      return (
         
-            <li className="m-3 max-w-sm bg-slate-300 pl-3 pb-1">
+            <li className="m-3 max-w-sm bg-red-100 pl-3 pb-1">
                 <h2 className="text-xl font-sans font-bold " fon >{name}</h2>
                 <div>Buy {quantity} in {category}</div>
             </li>   
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="m-1">
       <div>
-        <h2 className=" m-2 font-extrabold text-3xl text-slate-900">
+        <h2 className=" m-2 font-semibold text-2xl text-slate-900">
           Shopping List  | <a href="/"> Home Page</a>
         </h2>
         <ItemList />
