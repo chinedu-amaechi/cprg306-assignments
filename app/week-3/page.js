05579ba2e5639e767e74db981ec9f8d2f2cf43e9
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-05 18:08:46 -0600

    Week-3
    Item-List main page Styling with hover effect

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     <main className="m-1">
       <div>
         <h2 className=" m-2 font-semibold text-2xl text-slate-900">
-          Shopping List  | <a href="/"> Home Page</a>
+          Shopping List  | <a href="/" className="hover:text-red-300"> Home Page</a>
         </h2>
         <ItemList />
       </div>
