bc5b05f8d468da59f90fb027760e432896f61304
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-10 18:50:44 -0600

    Adding Shopping List Form
    An input field for the item name.
    A category dropdown with various options like Produce, Dairy, Bakery, and more.
    An "Add Item" button to submit the form.

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ export default function Page() {
       <div className="m-3">
       <p className="font-semibold">DEMOs for CPRG306 Assignment</p>
         <div className="flex w-full m-2 ml-0">
-          <ul className="flex w-full gap-4 text-center font-semibold text-red-500 p-1 pl-0 border">
-            <li className="border-2 border-blue-950 px-8 py-2"><a href="/week-2">Week 2</a></li>
-            <li className="border-2 border-blue-950 px-8 py-2"><a href="/week-3">Week 3</a></li>
-            <li className="border-2 border-blue-950 px-8 py-2"><a href="/week-4">Week 4</a></li>
-            <li className="border-2 border-blue-950 px-8 py-2"><a href="/week-5">Week 5</a></li>
+          <ul className="flex w-full gap-4 text-center font-semibold text-red-500 p-1 pl-0">
+            <li className="border-2 border-blue-950 px-6 py-2"><a href="/week-2">Week 2</a></li>
+            <li className="border-2 border-blue-950 px-6 py-2"><a href="/week-3">Week 3</a></li>
+            <li className="border-2 border-blue-950 px-6 py-2"><a href="/week-4">Week 4</a></li>
+            <li className="border-2 border-blue-950 px-6 py-2"><a href="/week-5">Week 5</a></li>
           </ul>
         </div>  
       </div>
--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -2,7 +2,8 @@
 import { useState } from "react";
 
 
-export default function NewItem() {
+export default function NewItem() 
+{
     const [quantity, setQuantity] = useState(1);
     
     const increment = () => {
@@ -17,19 +18,53 @@ export default function NewItem() {
       }
     };
 
+
+    <div>
+      <input>    </input>
+    </div>
+
   return (
-    <div className="flex items-center justify-between p-2 m-3 bg-slate-600 text-slate-950 w-36">
+    <form className="border border-black m-4 ">
 
-        <span className="text-white text-xl">{quantity}</span>
+        <input type="text" className=" border border-blue-300 m-3" placeholder="Item name.." />
 
-        <div className="flex items-center gap-2">
-          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={decrement} disabled={quantity==1}>-</button>
-      
-          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={increment} disabled={quantity==20}>+</button>
+        <div className="flex justify-center w-full">
+            <div className="flex items-center justify-between p-2 m-3 bg-slate-600 text-slate-950 w-36">
+
+                <span className="text-white text-xl">{quantity}</span>
+
+                <div className="flex items-center gap-2">
+                  <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={decrement} disabled={quantity==1}>-</button>
+              
+                  <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={increment} disabled={quantity==20}>+</button>
+                </div> 
+            </div>
+
+            <div>
+                <select
+                      className="bg-slate-200 text-black p-2 m-3 rounded-md">
+                      <option value="select">Select Category</option>
+                      <option value="produce">Produce</option>
+                      <option value="dairy">Dairy</option>
+                      <option value="bakery">Bakery</option>
+                      <option value="meat">Meat</option>
+                      <option value="frozen foods">Frozen Foods</option>
+                      <option value="canned goods">Canned Goods</option>
+                      <option value="dry goods">Dry Goods</option>
+                      <option value="beverages">Beverages</option>
+                      <option value="sncaks">Snacks</option>
+                      <option value="household">Household</option>
+                      <option value="other">Other</option>
+                </select>
+            </div>
 
         </div>
-         
-    </div>
+
+        <button className="bg-blue-400 m-3">Add Item</button>
         
-  );
+        
+    </form>
+
+ );
+
 }
