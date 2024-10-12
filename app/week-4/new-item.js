7b4fb6865a6d32b509108055a999429f2ccf63d6
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-12 13:39:58 -0600

    Form editing
    Tailwind styling

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -18,18 +18,20 @@ export default function NewItem() {
     };
 
   return (
-    <div className="flex items-center justify-between p-2 m-3 bg-slate-600 text-slate-950 w-36">
+    <div className="p-2 m-4 rounded-md w-36 bg-white border border-gray-600 text-slate-950">
+      <div className="flex justify-between items-center">
+        <span className="text-black text-xl">{quantity}</span>
+        <div className="flex gap-2">
+          <button
+            className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
+            onClick={decrement} disabled={quantity == 1}>-</button>
 
-        <span className="text-white text-xl">{quantity}</span>
-
-        <div className="flex items-center gap-2">
-          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={decrement} disabled={quantity==1}>-</button>
-      
-          <button className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75" onClick={increment} disabled={quantity==20}>+</button>
-
+          <button
+            className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
+            onClick={increment} disabled={quantity == 20}>+</button>
         </div>
-         
     </div>
+  </div>
         
   );
 }
--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -21,33 +21,31 @@ export default function NewItem() {
     
 
   return (
-    <form className="border border-black p-2 m-4 max-w-sm w-full" onSubmit={(e) => e.preventDefault()}>
+    <form className="border border-black bg-gray-100 p-2 m-4 max-w-sm w-full" onSubmit={(e) => e.preventDefault()}>
       <div className="mb-2">
         <input
           type="text"
-          className="rounded-md w-full mt-1 border border-black-500 p-2"
+          className="rounded-md w-full mt-1 border border-gray-300 p-2"
           placeholder="Item name"
         />
       </div>
 
-      
-      <div className="p-2 mt-1 mb-1 rounded-md w-36 bg-slate-600 text-slate-950">
-        <div className="flex justify-between">
-          <span className="text-white text-xl">{quantity}</span>
-        
-          <div className="flex">
-            <button
-              className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
-              onClick={decrement} disabled={quantity == 1}>-</button>
+      <div className="flex justify-between">
+        <div className="p-2 mt-1 mb-1 rounded-md w-36 bg-white border border-gray-300 text-slate-950">
+          <div className="flex justify-between items-center">
+            <span className="text-black text-xl">{quantity}</span>
+            <div className="flex gap-2">
+              <button
+                className="w-8 bg-blue-500 text-white font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
+                onClick={decrement} disabled={quantity == 1}>-</button>
 
-            <button
-              className="w-8 bg-blue-500 text-black font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
-              onClick={increment} disabled={quantity == 20}>+</button>
+              <button
+                className="w-8 bg-blue-500 text-white font-bold rounded-lg shadow-md hover:bg-red-200 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-opacity-75"
+                onClick={increment} disabled={quantity == 20}>+</button>
+            </div>
           </div>
-      </div>
-
-      <div>
-        <select className="bg-slate-200 text-black p-2 m-3 rounded-md ">
+        </div>
+        <select className="p-2 mt-1 mb-1 border-gray-300 border text-black rounded-md">
           <option value="produce">Produce</option>
           <option value="dairy">Dairy</option>
           <option value="bakery">Bakery</option>
@@ -56,15 +54,12 @@ export default function NewItem() {
           <option value="canned goods">Canned Goods</option>
           <option value="dry goods">Dry Goods</option>
           <option value="beverages">Beverages</option>
-          <option value="sncaks">Snacks</option>
+          <option value="snacks">Snacks</option>
           <option value="household">Household</option>
           <option value="other">Other</option>
         </select>
       </div>
-    </div>
-
-      <button type="submit" className="w-full mt-4 py-2 px-4 bg-blue-400 text-white rounded-md shadow-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-200 focus:ring-opacity-55">Add Item</button>
+      <button type="submit" className="w-full mt-2 py-2 px-4 bg-blue-500 text-white text-base rounded-md shadow-md hover:bg-blue-800 focus:ring-2 focus:ring-blue-200 focus:ring-opacity-55">Add Item</button>
     </form>
   );
-
 }
