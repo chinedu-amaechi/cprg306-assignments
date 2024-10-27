dd3bad214d42208de01ffa40ae42307e52846c86
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-27 02:24:36 -0600

    week-6 content created and updated

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -1,7 +1,7 @@
 
 function Item({name, quantity, category}) {
      return (
-            <li className="m-3 max-w-sm bg-red-100 pl-3 pb-1 capitalize hover:bg-red-200 hover:scale-105 hover:border-none transition-all duration-300">
+            <li className="m-3 max-w-sm bg-red-100 pl-3 pb-1 capitalize hover:bg-red-200 hover:scale-105 hover:border-none transition-all duration-300 cursor-pointer">
                 <h2 className="text-xl font-sans font-bold mb-3">{name}</h2>
                 <p className="text-sm">Quantity: {quantity}</p>
                 <p className="text-sm">Category: {category}</p>
