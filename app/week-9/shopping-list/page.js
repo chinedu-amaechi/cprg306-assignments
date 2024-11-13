60a6059016b6c069e406212a9ac5aeabfb905c42
Chinedu Amaechi <chymez56@yahoo.com>
2024-11-12 17:12:54 -0700

    Creating week-9

---
+++ app/week-9/shopping-list/page.js
@@ -0,0 +1,51 @@
+"use client";
+import Link from "next/link";
+
+import { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+
+
+function Page() {
+    const [items, setItems] = useState(itemsData);
+    const [ selectedItemName, setSelectedItemName ] = useState("");
+
+    const handleAddItem = (newItem) => {
+        setItems((prevItems) => [...prevItems, newItem]);
+    };
+
+    const handleItemSelect = (itemName) => {
+        const cleanName = itemName
+            .split(",")[0]
+            .trim()
+            .toLowerCase()
+            .replace(/[^a-zA-Z ]/g, "");
+        setSelectedItemName(cleanName);
+    };
+
+
+    return (
+        <main className='container mx-auto px-4 py-8'>
+            <h2 className=" m-2 font-semibold text-2xl text-slate-900 mb-4">
+                Shopping List  |
+                <Link href="/" className="hover:text-red-300"> Home Page</Link>
+            </h2>
+
+            <div className='mb-8'>
+                <NewItem onAddItem={handleAddItem} />
+            </div>
+            <div className="flex flex-row gap-10">
+                <div>
+                    <ItemList items={items} />
+                </div>
+                <div>
+                    <MealIdeas ingredient={selectedItemName} />
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Page;
